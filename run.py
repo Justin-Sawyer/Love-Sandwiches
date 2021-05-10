89b571fb8727fc14ebb28974bd493684537013a6
Justin-Sawyer <justin.sawyer@icloud.com>
2021-05-10 15:39:43 +0000

    Collect sales data from last 5 markets

--- run.py
+++ run.py
@@ -30,7 +30,7 @@ def get_sales_data():
         data_str = input("Enter your data here: ")
 
         sales_data = data_str.split(',')
- 
+
         if validate_data(sales_data):
             print("Data is valid!")
             break
@@ -57,10 +57,11 @@ def validate_data(values):
     return True
 
 
+"""
 def update_sales_worksheet(data):
-    """
-     Update sales worksheet, add new row with the list data provided
-    """
+
+    # Update sales worksheet, add new row with the list data provided
+
     print("Updating sales worksheet...\n")
     sales_worksheet = SHEET.worksheet('sales')
     sales_worksheet.append_row(data)
@@ -68,13 +69,25 @@ def update_sales_worksheet(data):
 
 
 def update_surplus_worksheet(data):
-    """
-     Update surplus worksheet, add new row with the list data provided
-    """
+
+    # Update surplus worksheet, add new row with the list data provided
+
     print("Updating surplus worksheet...\n")
     surplus_worksheet = SHEET.worksheet('surplus')
     surplus_worksheet.append_row(data)
     print("Surplus worksheet updated successfully.\n")
+"""
+
+
+def update_worksheet(data, worksheet):
+    """
+    Receives a list of integers to be inserted into a worksheet
+    Update the relevant worksheet with the data provided
+    """
+    print(f"Updating {worksheet} worksheet... \n")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} worksheet updated successfully.\n")
 
 
 def calculate_surplus_data(sales_row):
@@ -93,8 +106,21 @@ def calculate_surplus_data(sales_row):
     for stock, sales in zip(stock_row, sales_row):
         surplus = int(stock) - sales
         surplus_data.append(surplus)
-    
+
     return surplus_data
+
+
+def get_last_5_entries_sales():
+    """
+    """
+    sales = SHEET.worksheet('sales')
+
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+    
+    return columns
 
 
 def main():
@@ -103,10 +129,11 @@ def main():
     """
     data = get_sales_data()
     sales_data = [int(num) for num in data]
-    update_sales_worksheet(sales_data)
+    update_worksheet(sales_data, 'sales')
     new_surplus_data = calculate_surplus_data(sales_data)
-    update_surplus_worksheet(new_surplus_data)
+    update_worksheet(new_surplus_data, 'surplus')
 
 
 print("Welcome to Love Sandwiches Data Automation.\n")
-main()
+# main()
+sales_columns = get_last_5_entries_sales()
